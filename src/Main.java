b7f844df2c36ba7a6365d3e18cc69d588a7ac5ac
Luciano <luciano.luiz.riquelme@gmail.com>
2023-07-10 17:16:37 -0300

    agregando la logica del conversor de longitud

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         boolean continuar = true;
 
         while (continuar){
 
             // Mostrar un cuadro de diálogo de opción y obtener la opción seleccionada
-            Object[] opciones = {"Conversor de moneda", "Conversor de temperatura"};
+            Object[] opciones = {"Conversor de moneda", "conversor de Longitud"};
             int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opción de conversión", "Selector de opciones",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
             System.out.println("Opción seleccionada: " + opciones[seleccion]);
@@ -82,15 +82,66 @@ public class Main {
                 }
 
             }
+
             else if (seleccion == 1) {
-                // Lógica para el conversor de temperatura
-                // Implementa aquí el código para el conversor de temperatura
+                // Lógica para el conversor de longitud
+                Object[] opciones2 = {"Metros a Pies", "Pies a Metros"};
+                int seleccion2 = JOptionPane.showOptionDialog(null, "Seleccione una opción de conversión", "Selector de opciones",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones2, opciones2[0]);
+                System.out.println("Opción seleccionada: " + opciones2[seleccion2]);
+
+
+
+
+
+                if (seleccion2 == 0) {
+
+
+                    // Conversor de Metros a Pies
+                    double metros = obtenerValorDouble("Ingrese el valor en metros:");
+                    double pies = metros * 3.281;
+                    JOptionPane.showMessageDialog(null, metros + " metros equivale a " + pies + " pies.", "Conversión", JOptionPane.INFORMATION_MESSAGE);
+                } else if (seleccion2 == 1) {
+
+
+                    // Conversor de Pies a Metros
+                    double pies = obtenerValorDouble("Ingrese el valor en pies:");
+                    double metros = pies / 3.281;
+                    JOptionPane.showMessageDialog(null,
+                            pies + " pies equivale a " + metros + " metros.",
+                            "Conversión", JOptionPane.INFORMATION_MESSAGE);
+                }
+
+
+                // Mostrar un cuadro de diálogo de confirme si desea o no continuar
+                int opcion = JOptionPane.showConfirmDialog(null, "¿Deseas continuar?");
+                if (opcion != JOptionPane.YES_OPTION) {
+                    continuar = false;
+                }
+
+
             }
 
         }
 
         // Mostrar el mensaje "Programa finalizado" si el usuario no desee continuar
         JOptionPane.showMessageDialog(null, "programa finalizado");
+    }
+
+
+    //aqui creo un  metodo que sera llamado dentro del metodo main
+    public static double obtenerValorDouble(String mensaje) {
+        while (true) {
+            try {
+                String valorStr = JOptionPane.showInputDialog(null, mensaje);
+                double valor = Double.parseDouble(valorStr);
+                return valor;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Valor inválido. Ingrese solo números.",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 
 
